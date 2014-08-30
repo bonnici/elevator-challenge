56d4af3ae04b50ed88c172ae0c8a7a675aa739d9
bonnici <nickmarsh@gmail.com>
2014-08-30 20:49:05 +0000

    Basic bootstrap/angular skeleton

---
+++ js/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('elevatorSim', [
+	'elevatorSim.controllers'
+]);
---
+++ js/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('elevatorSim.controllers', []).
+
+controller('IndexController', ['$scope', function($scope) {
+	$scope.testScope = "testing scope";
+}]);
